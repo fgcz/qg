185172cacc0c9f47b36681ee2eb20dc42ed25095
Christian Panse <cp@fgcz.ethz.ch>
2025-10-02 11:26:42 +0200

    Mclass LCDevices Dia working

--- R/configProteomicsMclassVialXCaliburLCDevices.R
+++ R/configProteomicsMclassVialXCaliburLCDevices.R
@@ -1,3 +1,120 @@
 #R
 ## DevDay Claudia/Christian 20251002
 
+#' autoQC01 Mclass Vial XCalibur SII
+#'
+#' @inheritParams .autoQC01VialXCaliburSII
+#'
+#' @author Christian Panse <cp@fgcz.ethz.ch> 2025-08-27
+#' @return \code{data.frame} object
+.autoQC01MclassVialXCaliburLCDevices <- function(x, plateId = "1", QCrow = "F", QCcol = 8,
+                                           mode = "", containerid = "", lc = "Vanquish"){
+  
+  # plateId <- '1'
+  message(paste0("autoQC01 Mlass Vial XCalibur SII lc = ", lc, 'hard coded plaeId = ', plateId))
+  
+  data.frame(matrix(NA, ncol = ncol(x), nrow = 1)) -> pool
+  colnames(pool) <- colnames(x)
+  currentdate <- format(Sys.time(), "%Y%m%d")
+  
+  pool[1, "File Name"] <- sprintf("%s_@@@_C%s_autoQC01%s", currentdate, containerid, mode)
+  pool[1, "Position"] <- sprintf("%s:%s,%d", plateId, QCrow, QCcol)
+  pool[1, "Sample Name"] <- sprintf("autoQC01%s", mode)
+  
+  pool$`Inj Vol` <- 2
+  pool
+  
+}
+
+#' @inheritParams .autoQC01VialXCaliburSII
+.autoQC03MclassVialXCaliburLCDevices <- function(x, plateId = "1", QCrow = "F", QCcol = 7,
+                                           mode = "dia", containerid = "", lc = "Vanquish"){
+  data.frame(matrix(NA, ncol = ncol(x), nrow = 1)) -> pool
+  colnames(pool) <- colnames(x)
+  currentdate <- format(Sys.time(), "%Y%m%d")
+  
+  pool[1, "File Name"] <- sprintf("%s_@@@_C%s_autoQC03%s", currentdate, containerid, mode)
+  pool[1, "Position"] <- sprintf("%s:%s,%d", plateId, QCrow, QCcol)
+  pool[1, "Sample Name"] <- sprintf("autoQC03%s", mode)
+  
+  pool$`Inj Vol` <- 1
+  pool
+}
+
+#' @inheritParams .autoQC01VialXCaliburSII
+.cleanMclassVialXCaliburLCDevices <- function(x, plateId = "1", QCrow = "F", QCcol = 6,
+                                        mode = "", containerid = "", lc = "Vanquish"){
+  
+  data.frame(matrix(NA, ncol = ncol(x), nrow = 1)) -> pool
+  colnames(pool) <- colnames(x)
+  currentdate <- format(Sys.time(), "%Y%m%d")
+  
+  pool[1, "File Name"] <- sprintf("%s_@@@_C%s_clean%s", currentdate, containerid, mode)
+  pool[1, "Position"] <- sprintf("%s:%s,%d", plateId, QCrow, QCcol)
+  pool[1, "Sample Name"] <- sprintf("clean%s", mode)
+  
+  pool$`Inj Vol` <- 2
+  pool
+}
+
+
+
+#' Proteomics MCLASS Vial XCalibur SII
+#'
+#' @inheritParams .autoQC01VialXCaliburSII
+#'
+#' sample input see o39408
+#' 
+#' B:F,8 autoQC01
+#' B:F,7 autoQC03
+#' B:F,6 clean
+#' 
+#' 
+#' order 39330
+#' 
+#' @inheritParams qconfigMetabolomicsVanquishVialXCaliburSIIEquiSPLASH
+#' 
+#' @inherit qconfigMetabolomicsVanquishVialXCaliburSIIEquiSPLASH return 
+#' 
+#' @export
+#' 
+#' @author Christan Panse <cp@fgcz.ethz.ch> 2025-10-02
+qconfigProteomicsMclassVialXCaliburLCDevicesDia <- function(x, howOften = 8, ...){
+  shiny::showNotification("method qconfigProteomicsMclassVialXCaliburSII new implemented.",
+                          duration = 10, type = "warning")
+  
+  
+  cn <- c("File Name", "Path", "Position", "Inj Vol", "L3 Laboratory",
+          "Sample ID", "Sample Name", "Instrument Method")
+  
+  # base::save(x, file="/tmp/mx.RData")
+  # browser()
+  # ignore F (last) row TODO(cp): how to generalize it?
+  x[grepl(pattern = ":[ABCDEFG][,]{0,1}[1-9]", x = x$Position), ] -> x
+  
+  im <- paste0(x$Path[1], "\\methods\\")
+  
+  x |> .insertSample(howOften = howOften, path = x$Path[1], sampleFUN = .autoQC01MclassVialXCaliburLCDevices, ...) -> x
+  x |> .insertSample(howOften = round(howOften/2), path = x$Path[1], sampleFUN = .cleanMclassVialXCaliburLCDevices, ...) -> x
+  
+  # START
+  #x |> .insertSample(where = 0, path = x$Path[1], sampleFUN = .autoQC01MclassVialXCaliburSII, ...) -> x
+  #x |> .insertSample(where = 0, path = x$Path[1], sampleFUN = .autoQC03MclassVialXCaliburSII, ...) -> x
+  
+  # END
+  #x |> .insertSample(where = (nrow(x) + 1), path = x$Path[1], sampleFUN = .cleanMclassVialXCaliburSII, ...) -> x
+  #x |> .insertSample(where = (nrow(x) + 1), path = x$Path[1], sampleFUN = .autoQC01MclassVialXCaliburSII, ...) -> x
+  #x |> .insertSample(where = (nrow(x) + 1), path = x$Path[1], sampleFUN = .autoQC03MclassVialXCaliburSII, ...) -> x
+  ##x |> .insertSample(where = (nrow(x) + 1), path = x$Path[1], sampleFUN = .cleanMclassVialXCaliburSII, ...) -> x
+  
+  
+  
+  x$`L3 Laboratory` <- "FGCZ"
+  # x$Position |> sapply(FUN = .parsePlateNumber) -> x$Position
+  x$`Instrument Method` <- im
+  # x$Position |> sapply(FUN = .parsePlateNumber) -> x$Position
+  
+  gsub(pattern = ',', replacement = '', x = x$Position) -> x$Position
+  
+  return(x[, cn])
+}
