e9f899285d65cb14df0a909d5fbd16d62146ec36
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2026-02-20 15:30:30 +0100

    Auto upload result functionality

--- src/qg/apps/_bfabric_auth.py
+++ src/qg/apps/_bfabric_auth.py
@@ -34,21 +34,23 @@ def _generate_random_secret_key():
 
 
 class _AppConfig(BaseSettings):
-    validation_bfabric_instance: str = "https://fgcz-bfabric.uzh.ch/bfabric/"
-    supported_bfabric_instances: list[str] = [
-        "https://fgcz-bfabric.uzh.ch/bfabric/",
-        "https://fgcz-bfabric-test.uzh.ch/bfabric/",
-    ]
+    validation_bfabric_instance: str
+    supported_bfabric_instances: list[str]
     secret_key: SecretStr = Field(default_factory=_generate_random_secret_key)
+    feeder_user_credentials: dict[str, BfabricAuth]
 
 
 class BfabricUser(BaseUser):
     """User object that carries the bfabric session fields."""
 
-    def __init__(self, login: str, password: str, instance: str) -> None:
+    def __init__(
+        self, login: str, password: str, instance: str, feeder_login: str | None, feeder_password: str | None
+    ) -> None:
         self.bfabric_auth_login = login
         self.bfabric_auth_password = password
         self.bfabric_instance = instance
+        self.feeder_login = feeder_login
+        self.feeder_password = feeder_password
 
     @property
     def is_authenticated(self) -> bool:
@@ -62,9 +64,16 @@ class BfabricUser(BaseUser):
     def identity(self) -> str:
         return self.bfabric_auth_login
 
-    def get_bfabric_client(self) -> Bfabric:
+    def get_bfabric_user_client(self) -> Bfabric:
         config = ConfigData(
             auth=BfabricAuth(login=self.bfabric_auth_login, password=self.bfabric_auth_password),
+            client=BfabricClientConfig(base_url=self.bfabric_instance),
+        )
+        return Bfabric(config)
+
+    def get_bfabric_feeder_client(self) -> Bfabric:
+        config = ConfigData(
+            auth=BfabricAuth(login=self.feeder_login, password=self.feeder_password),
             client=BfabricClientConfig(base_url=self.bfabric_instance),
         )
         return Bfabric(config)
@@ -73,14 +82,24 @@ class BfabricUser(BaseUser):
 class _SessionAuthBackend(AuthenticationBackend):
     """Authentication backend that reads bfabric_session dict from session."""
 
+    def __init__(self, app_config: _AppConfig) -> None:
+        super().__init__()
+        self._app_config = app_config
+
     async def authenticate(self, conn: HTTPConnection) -> tuple[AuthCredentials, BfabricUser] | None:
         bfabric_session = conn.scope.get("session", {}).get("bfabric_session")
         if not bfabric_session or not isinstance(bfabric_session, dict):
             return None
+
+        # obtain the feeder user
+        feeder_user = self._app_config.feeder_user_credentials.get(bfabric_session["bfabric_instance"])
+
         user = BfabricUser(
             login=str(bfabric_session["bfabric_auth_login"]),
             password=str(bfabric_session["bfabric_auth_password"]),
             instance=str(bfabric_session["bfabric_instance"]),
+            feeder_login=feeder_user.login if feeder_user is not None else None,
+            feeder_password=feeder_user.password if feeder_user is not None else None,
         )
         creds = AuthCredentials(["authenticated"])
         return creds, user
@@ -92,7 +111,7 @@ def create_bfabric_fastapi_app(marimo_app_path: Path) -> FastAPI:
     app_config = _AppConfig()
     token_validator = create_bfabric_validator(settings=app_config)
 
-    app.add_middleware(AuthenticationMiddleware, backend=_SessionAuthBackend())
+    app.add_middleware(AuthenticationMiddleware, backend=_SessionAuthBackend(app_config=app_config))
     app.add_middleware(
         BfabricAuthMiddleware,
         token_validator=token_validator,
